0675979fdca93d96f8979327cd04b1894952c0f4
Alexandre Coninx <a.coninx@imperial.ac.uk>
2014-03-05 16:03:46 +0000

    It works :)

--- markov-twitter.py
+++ markov-twitter.py
@@ -7,24 +7,57 @@
 
 from markovstate import MarkovState
 import time
+import fileinput
+import glob
+import os
+import random
+
 
 topics = ("evolution","Darwin","natural selection","survival of the fittest")
+
+sources = ["twitter.txt"]
+
+
+def get_generator_for_files(files):
+	paths = [path
+		for ps in files
+		for path in glob.glob(os.path.expanduser(ps))]
+
+	def charinput(paths):
+		with fileinput.input(paths) as fi:
+			for line in fi:
+				for char in line:
+					yield char
+	return charinput(paths)
+			
+
+
+def get_characters_about(chain, prefix, nmax=140):
+	# Abide by the 140 charcters limit
+	sentence = prefix+" "
+	s = int(time.time())
+	chunk = chain.generate(chunks=1, seed=s, prefix=tuple(prefix.split()))
+	sentence += chunk
+	while True:
+		chunk = chain.more()
+		if(len(sentence) + len(chunk) + 1 > nmax):
+			break
+		else:
+			sentence += (" "+chunk)
+	return sentence
+
 
 
 
 
 def main():
 	chain = MarkovState()
-
 	print("Initializing Markov chain...")
-	trainingdata = open("twitter.txt")
-	chain.train(2,trainingdata,True)
-	trainingdata.close()
-	chain.dump("foo.markov")
+	chain.train(2,get_generator_for_files(sources),noparagraphs=True)
 	print("Done init. Testing:")
 	for topic in topics:
 		print ("According to Twitter, here is some stuff about "+topic+":")
-		print (chain.generate(5))
+		print(get_characters_about(chain, topic)+"\n")
 		time.sleep(1)
 	
 
