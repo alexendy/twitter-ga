4f804aec3cf9bb28ca68f9105506bd224fb41d4b
Alexandre Coninx <a.coninx@imperial.ac.uk>
2014-03-10 16:42:44 +0000

    Python3 twitter processing
    - add Twitter submodule from https://github.com/sixohsix/twitter
    - use it in dump-twitter-data.py

--- dump-twitter-data.py
+++ dump-twitter-data.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 """ Alexandre Coninx
     Imperial College London, 04/03/2014
 """ 
 
-import twitter
+import importlib.machinery
 import codecs
+import sys
+
+sys.path.append("lib/twitter")
+
+from twitter import *
+
 
 topics = ("evolution","Darwin","natural selection","survival of the fittest")
 
@@ -14,19 +20,21 @@ filename = "twitter.txt"
 
 
 def main():
-	api =  twitter.Api(consumer_key='XKQ0jsxPiDcUuGZjU2nYSA',
-			consumer_secret='jU4xkJlOg9j3tQi3UCY80TVUGu4MBsHjXpVSD31TQY',
-			access_token_key='2349861931-AuTecfyzW3aSvinkywuD65RHD4U1vhYktePSEOi',
-			access_token_secret='84dFSLjIfj8f2894tzLEXlUFbRhGPA4qeCEAGo1CW2Rx7')
+	api =  Twitter(auth=OAuth('2349861931-AuTecfyzW3aSvinkywuD65RHD4U1vhYktePSEOi', # OAUTH_TOKEN
+			'84dFSLjIfj8f2894tzLEXlUFbRhGPA4qeCEAGo1CW2Rx7', # OAUTH_SECRET
+			'XKQ0jsxPiDcUuGZjU2nYSA', # CONSUMER_KEY
+			'jU4xkJlOg9j3tQi3UCY80TVUGu4MBsHjXpVSD31TQY')) # CONSUMER_SECRET
+			
 
 	f = open(filename,'w')
 	for topic in topics:
-		tweets = api.GetSearch(term=topic, lang='en', count=1000)
+		r = api.search.tweets(q=topic,lang='en',count=150)
+		tweets = r['statuses']
 		print(str(len(tweets))+" tweets found about "+topic)
 		for t in tweets:
 			# Brutally convert to ASCII
-			asciidata=t.text.encode("ascii","ignore")
-			f.write(asciidata)
+			asciidata=t['text'].encode('UTF-8').decode("ascii","ignore")
+			f.write(asciidata+'\n')
 	f.close()
 	
 
